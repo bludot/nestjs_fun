9cf89682dba6abb396fcfcc140edcef016aaeeff
James Trotter <admin@floretos.com>
2020-09-29 20:51:17 +0700

    some notes on organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,24 @@ import { PetsModule } from './pets/pets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo1/nest'), CatsModule, DogsModule, PetsModule],
+  /**
+   * everything is singleton according to nestjs
+   * when we import a module, nestjs makes sure its not recreated
+   *
+   * how do we split?
+   *  Each "module" is a "domain". Only what is relevant should be in there.
+   *  modules will have their own services and controllers as well as their
+   *  own schemas. ex: cats shouldnt be touching dogs in database but pets
+   *  can and we do this by exporting the dogs and cats services. Please
+   *  reference also https://hanamirb.org/ as they have a similar idea
+   *
+   */
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo1/nest'),
+    CatsModule,
+    DogsModule,
+    PetsModule
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
